f1fcc01e4290a39fa4ccb4b83effb238624b6022
Denis Denisov <d.denisov@rambler-co.ru>
2018-12-08 18:14:43 +0300

    WIP Day 6 part 1

--- src/lib/6/index.test.ts
+++ src/lib/6/index.test.ts
@@ -1,14 +1,14 @@
-import { drawGrid } from "./";
+import { setCoorditates, getClosests, fillClosests } from "./";
 
 test("examples", () => {
-  expect(
-    drawGrid(`1, 1
+  const { grid, coordinates } = setCoorditates(`1, 1
 1, 6
 8, 3
 3, 4
 5, 5
-8, 9`)
-  ).toBe(`..........
+8, 9`);
+
+  expect(grid.map(r => r.join("")).join("\n")).toBe(`..........
 .A........
 ..........
 ........C.
@@ -18,4 +18,27 @@ test("examples", () => {
 ..........
 ..........
 ........F.`);
+
+  expect(getClosests(coordinates, 0, 0)).toEqual(["A"]);
+  expect(getClosests(coordinates, 1, 0)).toEqual(["A"]);
+  expect(getClosests(coordinates, 4, 0)).toEqual(["A"]);
+  expect(getClosests(coordinates, 5, 0)).toEqual(["A", "E"]);
+  expect(getClosests(coordinates, 9, 0)).toEqual(["C"]);
+  expect(getClosests(coordinates, 3, 3)).toEqual(["D"]);
+  expect(getClosests(coordinates, 0, 4)).toEqual(["B", "D"]);
+
+  expect(
+    fillClosests(grid, coordinates)
+      .map(r => r.join(""))
+      .join("\n")
+  ).toBe(`aaaaa.cccc
+aAaaa.cccc
+aaaddecccc
+aadddeccCc
+..dDdeeccc
+bb.deEeecc
+bBb.eeee..
+bbb.eeefff
+bbb.eeffff
+bbb.ffffFf`);
 });
--- src/lib/6/index.ts
+++ src/lib/6/index.ts
@@ -1,8 +1,40 @@
-export function drawGrid(input: string) {
-  let grid: Array<Array<string>> = [[]];
-  let maxX = 0;
-  let maxY = 0;
-  const coordinates: { [name: string]: [number, number] } = {};
+type Grid = Array<Array<string>>;
+type Coordinates = {
+  [name: string]: [number, number];
+};
+
+export function getClosests(
+  coordinates: Coordinates,
+  x: number,
+  y: number
+): Array<string> {
+  let closests: Array<string> = [];
+  let min = Infinity;
+  for (let c in coordinates) {
+    let length =
+      Math.abs(coordinates[c][0] - x) + Math.abs(coordinates[c][1] - y);
+
+    if (length < min) {
+      closests = [c];
+      min = length;
+    } else if (length === min) {
+      closests.push(c);
+    }
+  }
+  return closests;
+}
+
+export function setCoorditates(
+  input: string
+): { grid: Grid; coordinates: Coordinates } {
+  const grid: Grid = [[]];
+  const coordinates: Coordinates = {};
+  let maxX = -Infinity;
+  let maxY = -Infinity;
+
+  const onXs = new Map();
+  const onYs = new Map();
+
   input.split("\n").forEach((s, index) => {
     const coordinateName = String.fromCharCode(index + 65);
     const x = parseInt(s.split(",")[0]);
@@ -26,5 +58,26 @@ export function drawGrid(input: string) {
     }
   }
 
-  return grid.map(r => r.join("")).join("\n");
+  return {
+    grid,
+    coordinates
+  };
+}
+
+export function fillClosests(grid: Grid, coordinates: Coordinates): Grid {
+  const width = grid[0].length - 1;
+  const height = grid.length - 1;
+  for (let y = 0; y <= height; y++) {
+    for (let x = 0; x <= width; x++) {
+      if (grid[y] === undefined) {
+        grid[y] = [];
+      }
+      grid[y][x] = grid[y][x] || ".";
+      if (grid[y][x] === ".") {
+        let closests = getClosests(coordinates, x, y);
+        grid[y][x] = closests.length === 1 ? closests[0].toLowerCase() : ".";
+      }
+    }
+  }
+  return grid;
 }
